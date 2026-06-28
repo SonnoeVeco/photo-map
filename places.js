6512987357518db77dd538eae470ab4f5ed809f0
SonnoeVeko <106616421+SonnoeVeco@users.noreply.github.com>
2026-06-28 13:31:41 +0100

    Update places.js

--- places.js
+++ places.js
@@ -56,5 +56,14 @@ const places = [
   website: "https://www.visitshropshirehills.co.uk/shropshire-hills/long-mynd-and-stiperstones",
   base: "long-mynd-bur-way",
   count: 12
+},
+  {
+  name: "St Laurence's Church",
+  category: "Church",
+  coords: [52.5375, -2.8033],
+  description: "Standing in the heart of Church Stretton beneath the Long Mynd, St Laurence's Church is a historic parish church with origins dating back to the 12th century. Renowned for its striking sandstone tower, beautiful stained-glass windows, and peaceful churchyard, it has served the local community for centuries",
+  website: "https://stlaurenceschurchstretton.org.uk/",
+  base: "st-laurences-church",
+  count: 1
 }
 ];
